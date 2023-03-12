6492cad99d19c4d8cab7cc7e742a9298ac484cb8
tiffany <ting11222001@gmail.com>
2023-03-12 20:35:19 +1030

    feat: 🎸 create card grid

--- components/About.tsx
+++ components/About.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { SiHiveBlockchain, SiStrapi, SiFsecure } from 'react-icons/si';
+import { VscServerProcess } from 'react-icons/vsc';
 
 type Props = {}
 
@@ -10,6 +12,16 @@ const About = (props: Props) => {
         <p className='font-light text-lg py-4'>
           As a technology-driven trading firm we develop innovative technology and algorithms to trade in all major asset classes on more than 100 trading venues around the world.
         </p>
+        <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-4'>
+          <div className='flex flex-col border-2'>
+            <div><SiHiveBlockchain /></div>
+            <h3 className='text-2xl py-4'>Where cutting edge technology meets trading </h3>
+            <p className='font-light text-lg py-4'>We are as much a technology firm as a trading house. We invest significant time and resources to ensure our traders have the best tools for the job.</p>
+          </div>
+          <p>card</p>
+          <p>card</p>
+          <p>card</p>
+        </div>
       </div>
     </div>
   )
