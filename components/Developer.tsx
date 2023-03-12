65e73da772c36f5e3723995d7abf7b10b322d0fc
tiffany <ting11222001@gmail.com>
2023-03-12 21:44:57 +1030

    feat: 🎸 create developer section, replace content with why

---
+++ components/Developer.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+type Props = {}
+
+const Developer = (props: Props) => {
+  return (
+    <div className='w-full text-white bg-black text-center'>
+      <div className='max-w-[1240px] mx-auto px-4 py-16 md:flex'>
+        <div>
+          <h1>Why trade with us</h1>
+          <p className='font-light text-lg py-8'>
+            We make markets in multiple asset classes across <span className='text-green'>over 100</span> trading venues worldwide. We have <span className='text-green'>30 years</span> of industry-leading experience combining technology, market knowledge, and advanced risk management.
+          </p>
+        </div>
+        <div className='flex justify-center w-full'>
+          <img src='trade.png' alt="/" className='max-w-[250px] shadow-lg shadow-green' />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Developer
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
+import Developer from "@/components/Developer";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <About />
+      <Developer />
     </h1>
   )
 }
