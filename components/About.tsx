0ded9f5673648618b6c19f12a2a47087a7a462f9
tiffany <ting11222001@gmail.com>
2023-03-12 20:24:30 +1030

    feat: 🎸 create title in the about section

---
+++ components/About.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type Props = {}
+
+const About = (props: Props) => {
+  return (
+    <div className='max-w-[1240px] mx-auto px-4 py-16 bg-black text-white text-center'>
+      <div>
+        <h1 className='py-4'>ACCESS OUR LIQUIDITY</h1>
+        <p className='font-light text-lg py-4'>
+          As a technology-driven trading firm we develop innovative technology and algorithms to trade in all major asset classes on more than 100 trading venues around the world.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default About
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import About from "@/components/About";
 
 export default function Home() {
   return (
     <h1>
       <Navbar />
       <Hero />
+      <About />
     </h1>
   )
 }
