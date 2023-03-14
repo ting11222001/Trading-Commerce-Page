a4b6bd536c0564af3faecce0f126df4c8c1eca6c
tiffany <ting11222001@gmail.com>
2023-03-14 10:55:32 +1030

    fix: 🐛 padding

--- components/Subscribe.tsx
+++ components/Subscribe.tsx
@@ -10,7 +10,7 @@ const Subscribe = (props: Props) => {
         <input type="email" placeholder='Enter your email' className='font-light text-lg p-4 rounded-3xl mr-4' />
         <button className='ml-4 bg-gradient-to-r from-green to-purple hover:from-pink hover:to-yellow'>Sign up</button>
       </div>
-      <div className='flex justify-center items-center py-2'>
+      <div className='flex justify-center items-center p-2'>
         <input type="checkbox" className='mr-2' />
         <p className='font-light text-lg py-4'>Yes, I <span className='text-green'>agree</span> to receive email communications from TRADING.</p>
       </div>
